286faaad1f2079823473b026f56fbd343cc1b108
ShaheryarAyub <95630185+ShaheryarAyub@users.noreply.github.com>
2021-12-08 17:38:51 +0500

    Update Source_3.cpp

--- Source_3.cpp
+++ Source_3.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void check (char character)
+{
+    if ((character >= 65 && character <= 90)
+        || (character >= 97 && character <= 122))
+        cout << " Alphabet ";
+
+    else if (character >= 48 && character <= 57)
+        cout << " Digit ";
+
+    else
+        cout << " Special Character ";
+}
 int main()
 {
-    char character;
+    char c;
     cout << "Enter Character" << endl;
-    cin >> character;
-
-    {
-        if ((character >= 65 && character <= 90)
-            || (character >= 97 && character <= 122))
-            cout << " Alphabet ";
-
-        else if (character >= 48 && character <= 57)
-            cout << " Digit ";
-
-        else
-            cout << " Special Character ";
-    }
-
-    return 0;
+    cin >> c;
+    check(c);
 }
