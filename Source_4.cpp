dec8195a7c9e1f71cb0dea4d2b76c78227a812c0
ShaheryarAyub <95630185+ShaheryarAyub@users.noreply.github.com>
2021-12-13 12:58:50 +0500

    Update Source_4.cpp

--- Source_4.cpp
+++ Source_4.cpp
@@ -1,39 +1,86 @@
 #include <iostream>
 using namespace std;
-
-void calc (int num1, int num2, char op)
+class Calculator 
 {
-    switch (op)
+private:
+    int num_1;
+    int num_2;
+public:
+    void getnums();   
+    int add();
+    int sub();
+    int mul();
+    int div();
+    int rem();
+};
+
+void Calculator :: getnums()
+{
+    cout << "Enter First Number: ";
+    cin >> num_1;
+    cout << "Enter Second Number: ";
+    cin >> num_2;
+}
+
+int Calculator :: add()
+{
+    return num_1 + num_2;
+}
+
+int Calculator :: sub()
+{
+    return num_1 - num_2;
+}
+
+int Calculator :: mul()
+{
+    return num_1 * num_2;
+}
+
+ int Calculator::div()
+{
+    if (num_2 == 0)
     {
-    case '+':
-        cout << num1 + num2;
-        break;
-    case '-':
-        cout << num1 - num2;
-        break;
-    case '*':
-        cout << num1 * num2;
-        break;
-    case '/':
-        cout << num1 / num2;
-        break;
-    case '%':
-        cout << num1 % num2;
-        break;
-    default:
-        cout << "Error operator is not correct";
-        break;
+        cout << "Divison By Zero" << endl;
+        return INFINITY;
     }
-}    
+    else
+    {
+        return num_1 / num_2;
+    }
+}
+
+int  Calculator::rem()
+{
+    return num_1 % num_2;
+}
 
 int main() 
 {
-    char op;
-    int num_1, num_2;
-    cout << "Enter two numbers" << endl;
-    cin >> num_1 >> num_2;
-
+    Calculator cal;
+    cal.getnums();
+    char opp;
     cout << "Enter the operator" << endl;
-    cin >> op;
-    calc(num_1, num_2, op);
+    cin >> opp;   
+    
+    switch (opp) 
+    {
+    case '+':
+        cout << "Result: " << cal.add() << endl;
+        break;
+    case '-':      
+        cout << "Result: " << cal.sub() << endl;
+        break;
+    case '*':     
+        cout << "Result: " << cal.mul() << endl;
+        break;
+    case '/':       
+        cout << "Result: " << cal.div() << endl;
+        break;
+    case '%':
+        cout << "Result: " << cal.rem() << endl;
+        break;
+    }
+    return 0;
 }
+
