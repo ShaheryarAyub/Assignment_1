079492fe3fd139e7f801ce4368dcbabfc89ad99b
ShaheryarAyub <95630185+ShaheryarAyub@users.noreply.github.com>
2021-12-13 12:56:59 +0500

    Update Source_2.cpp

--- Source_2.cpp
+++ Source_2.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
 using namespace std;
 
-void PrintGrade(int marks)
-{    
-        if (marks < 0 || marks > 100)
-            cout << "invalid marks" << endl;
-        else if (marks >= 90)
-            cout << "Grade is A+" << endl;
-        else if (marks >= 80)
-            cout << "Grade is A" << endl;
-        else if (marks >= 70)
-            cout << "Grade is B" << endl;
-        else if (marks >= 60)
-            cout << "Grade is C" << endl;
-        else if (marks >= 50)
-            cout << "Grade is D" << endl;
-        else
-             cout << "Fail" << endl;  
+class Grade
+{
+public:
+    void PrintGrade(int);
+};
+
+void Grade :: PrintGrade(int marks)
+{
+    if (marks < 0 || marks > 100)
+        cout << "invalid marks" << endl;
+    else if (marks >= 90)
+        cout << "Grade is A+" << endl;
+    else if (marks >= 80)
+        cout << "Grade is A" << endl;
+    else if (marks >= 70)
+        cout << "Grade is B" << endl;
+    else if (marks >= 60)
+        cout << "Grade is C" << endl;
+    else if (marks >= 50)
+        cout << "Grade is D" << endl;
+    else
+        cout << "Fail" << endl;
 }
 
 int main()
-{ 
-    int marks;
-    cout << "Enter the Marks" << endl;
-    cin >> marks;
+{
+    Grade marks;
+    int m;
+    cout << "Enter the marks: ";
+    cin >> m;    
     while (1)
     {
-        PrintGrade(marks);
-        cout << "Enter the Marks" << endl;
-        cin >> marks;
+        marks.PrintGrade(m);
+        cout << "Enter the marks: ";
+        cin >> m;        
     }
 }
